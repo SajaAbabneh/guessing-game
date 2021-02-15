be57e6950b9dc4c4f5fe0991fcf8178e285983d9
SajaAbabneh <ababnehsaja@yahoo.com>
2021-02-15 22:14:20 +0200

    lab2

--- java/code.js
+++ java/code.js
@@ -1,6 +1,7 @@
 'use strict';
 
 
+
 let fName= prompt('what is your name');
 alert('Welcome ' +fName );
 
@@ -8,17 +9,14 @@ let uniSpecialization= prompt('Did you know i studied computer engineer? ','yes/
 //console.log(uniSpecialization);
 
 if(uniSpecialization !== 'yes' && uniSpecialization !=='y' && uniSpecialization !== 'no' && uniSpecialization !== 'n')
-{
-    uniSpecialization=prompt('Did you know i studied computer engineer? please answer yes or no  ','yes/y or no/n').toLocaleLowerCase();
+{uniSpecialization=prompt('Did you know i studied computer engineer? please answer yes or no  ','yes/y or no/n').toLocaleLowerCase();
 }
 
 if(uniSpecialization ==='yes' || uniSpecialization==='y')
-{
-    alert('Im here if you want anythings in computer ');
+{ alert('Im here if you want anythings in computer ');
 
 }else if (uniSpecialization ==='no' || uniSpecialization ==='n')
-{
-    alert('Okay! at the moment you are know !');
+{alert('Okay! at the moment you are know !');
 
 } 
 
@@ -28,17 +26,14 @@ let myAge= prompt('Did you know my age? ','yes/y or no/n').toLocaleLowerCase();
 //console.log(myAge);
 
 if( myAge !== 'yes' && myAge !=='y' && myAge !== 'no' && myAge !== 'n')
-{
-    myAge=prompt('Did you know my age ? please answer yes or no  ','yes/y or no/n').toLocaleLowerCase();
+{myAge=prompt('Did you know my age ? please answer yes or no  ','yes/y or no/n').toLocaleLowerCase();
 }
 
 if(myAge ==='yes' || myAge==='y')
-{
-    alert('Hope to me long life');
+{alert('Hope to me long life');
 
 }else if (myAge ==='no' || myAge ==='n')
-{
-    alert('Okay! my age is 24 !');
+{alert('Okay! my age is 24 !');
 
 }
 
@@ -48,17 +43,14 @@ let colorLove= prompt('Did you know my lovely color ? ','yes/y or no/n').toLocal
 //console.log(colorLove);
 
 if( colorLove !== 'yes' && colorLove !=='y' && colorLove !== 'no' && colorLove !== 'n')
-{
-    colorLove=prompt('Did you know my lovely color ? please answer yes or no  ','yes/y or no/n').toLocaleLowerCase();
+{colorLove=prompt('Did you know my lovely color ? please answer yes or no  ','yes/y or no/n').toLocaleLowerCase();
 }
 
 if(colorLove ==='yes' || colorLove==='y')
-{
-    alert('Great! what about you ?');
+{alert('Great! what about you ?');
 
 }else if (colorLove ==='no' || colorLove ==='n')
-{
-    alert('Okay! my lovely color is gray!');
+{alert('Okay! my lovely color is gray!');
 
 }
 
@@ -68,17 +60,14 @@ let favDrink= prompt('Did you know my favourite drink ? ','yes/y or no/n').toLoc
 //console.log(favDrink);
 
 if( favDrink !== 'yes' && favDrink !=='y' && favDrink !== 'no' && favDrink !== 'n')
-{
-    favDrink=prompt('Did you know my favourite drink ? please answer yes or no  ','yes/y or no/n').toLocaleLowerCase();
+{favDrink=prompt('Did you know my favourite drink ? please answer yes or no  ','yes/y or no/n').toLocaleLowerCase();
 }
 
 if(favDrink ==='yes' || favDrink==='y')
-{
-    alert('Okay then give me some of it');
+{alert('Okay then give me some of it');
 
 }else if (favDrink ==='no' || favDrink ==='n')
-{
-    alert('My favourite drink is Coffee!');
+{alert('My favourite drink is Coffee!');
 
 }
 
@@ -88,17 +77,14 @@ let loveflower= prompt('Did you know if i love flowers? ','yes/y or no/n').toLoc
 //console.log(loveflower);
 
 if( loveflower !== 'yes' && loveflower !=='y' && loveflower !== 'no' && loveflower !== 'n')
-{
-    loveflower=prompt('Did you know if i love flowers ? please answer yes or no  ','yes/y or no/n').toLocaleLowerCase();
+{loveflower=prompt('Did you know if i love flowers ? please answer yes or no  ','yes/y or no/n').toLocaleLowerCase();
 }
 
 if(loveflower ==='yes' || loveflower==='y')
-{
-    alert('You are flower!');
+{alert('You are flower!');
 
 }else if (loveflower ==='no' || loveflower ==='n')
-{
-    alert('Okay! i love flower so much espicially pink flower');
+{alert('Okay! i love flower so much espicially pink flower');
 
 }
 

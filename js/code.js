b9dcb7f9d46447c455db69628adb9298d1a5b187
SajaAbabneh <ababnehsaja@yahoo.com>
2021-02-17 16:02:15 +0200

    function

--- js/code.js
+++ js/code.js
@@ -8,90 +8,103 @@ let count = 0;
 
 let uniSpecialization = prompt('Did you know i studied computer engineer? ', 'yes/y or no/n').toLowerCase();
 //console.log(uniSpecialization);
+function major(university){
+  while (uniSpecialization !== 'yes' && uniSpecialization !== 'y' && uniSpecialization !== 'no' && uniSpecialization !== 'n') {
+    uniSpecialization = prompt('Did you know i studied computer engineer? please answer yes or no  ', 'yes/y or no/n').toLowerCase();
+  }
 
-while (uniSpecialization !== 'yes' && uniSpecialization !== 'y' && uniSpecialization !== 'no' && uniSpecialization !== 'n') {
-  uniSpecialization = prompt('Did you know i studied computer engineer? please answer yes or no  ', 'yes/y or no/n').toLowerCase();
+  if (uniSpecialization === 'yes' || uniSpecialization === 'y') {
+    alert('Im here if you want anythings in computer ');
+    return uniSpecialization;
+    count++;
+
+  } else if (uniSpecialization === 'no' || uniSpecialization === 'n') {
+    alert('Okay! at the moment you are know !');
+  }
 }
-
-if (uniSpecialization === 'yes' || uniSpecialization === 'y') {
-  alert('Im here if you want anythings in computer ');
-  count++;
-
-} else if (uniSpecialization === 'no' || uniSpecialization === 'n') {
-  alert('Okay! at the moment you are know !');
-
-}
+major(uniSpecialization);
 
 // second question
 
 let myAge = prompt('Did you know my age? ', 'yes/y or no/n').toLowerCase();
 //console.log(myAge);
+function myOld(old){
+  while (myAge !== 'yes' && myAge !== 'y' && myAge !== 'no' && myAge !== 'n') {
+    myAge = prompt('Did you know my age ? please answer yes or no  ', 'yes/y or no/n').toLowerCase();
+  }
 
-while (myAge !== 'yes' && myAge !== 'y' && myAge !== 'no' && myAge !== 'n') {
-  myAge = prompt('Did you know my age ? please answer yes or no  ', 'yes/y or no/n').toLowerCase();
+  if (myAge === 'yes' || myAge === 'y') {
+    alert('Hope to me long life');
+    count++;
+
+  } else if (myAge === 'no' || myAge === 'n') {
+    alert('Okay! my age is 24 !');
+
+  }
 }
-
-if (myAge === 'yes' || myAge === 'y') {
-  alert('Hope to me long life');
-  count++;
-
-} else if (myAge === 'no' || myAge === 'n') {
-  alert('Okay! my age is 24 !');
-
-}
+myOld(myAge);
 
 // third question
 
 let colorLove = prompt('Did you know my lovely color ? ', 'yes/y or no/n').toLowerCase();
 //console.log(colorLove);
+function loveColor(color){
 
-while (colorLove !== 'yes' && colorLove !== 'y' && colorLove !== 'no' && colorLove !== 'n') {
-  colorLove = prompt('Did you know my lovely color ? please answer yes or no  ', 'yes/y or no/n').toLowerCase();
+  while (colorLove !== 'yes' && colorLove !== 'y' && colorLove !== 'no' && colorLove !== 'n') {
+    colorLove = prompt('Did you know my lovely color ? please answer yes or no  ', 'yes/y or no/n').toLowerCase();
+  }
+
+  if (colorLove === 'yes' || colorLove === 'y') {
+    alert('Great! what about you ?');
+    count++;
+
+  } else if (colorLove === 'no' || colorLove === 'n') {
+    alert('Okay! my lovely color is gray!');
+
+  }
 }
-
-if (colorLove === 'yes' || colorLove === 'y') {
-  alert('Great! what about you ?');
-  count++;
-
-} else if (colorLove === 'no' || colorLove === 'n') {
-  alert('Okay! my lovely color is gray!');
-
-}
+loveColor(colorLove);
 
 // question four
 
 let favDrink = prompt('Did you know my favourite drink ? ', 'yes/y or no/n').toLowerCase();
 //console.log(favDrink);
+function favoutriteDrink(drink){
 
-while (favDrink !== 'yes' && favDrink !== 'y' && favDrink !== 'no' && favDrink !== 'n') {
-  favDrink = prompt('Did you know my favourite drink ? please answer yes or no  ', 'yes/y or no/n').toLowerCase();
+  while (favDrink !== 'yes' && favDrink !== 'y' && favDrink !== 'no' && favDrink !== 'n') {
+    favDrink = prompt('Did you know my favourite drink ? please answer yes or no  ', 'yes/y or no/n').toLowerCase();
+  }
+
+  if (favDrink === 'yes' || favDrink === 'y') {
+    alert('Okay then give me some of it');
+    count++;
+
+  } else if (favDrink === 'no' || favDrink === 'n') {
+    alert('My favourite drink is Coffee!');
+
+  }
 }
-
-if (favDrink === 'yes' || favDrink === 'y') {
-  alert('Okay then give me some of it');
-  count++;
-
-} else if (favDrink === 'no' || favDrink === 'n') {
-  alert('My favourite drink is Coffee!');
-
-}
+favoutriteDrink(favDrink);
 
 // question five
 
 let loveflower = prompt('Did you know if i love flowers? ', 'yes/y or no/n').toLowerCase();
 //console.log(loveflower);
 
-while (loveflower !== 'yes' && loveflower !== 'y' && loveflower !== 'no' && loveflower !== 'n') {
-  loveflower = prompt('Did you know if i love flowers ? please answer yes or no  ', 'yes/y or no/n').toLowerCase();
-}
+function flowerLove(flower){
+  while (loveflower !== 'yes' && loveflower !== 'y' && loveflower !== 'no' && loveflower !== 'n') {
+    loveflower = prompt('Did you know if i love flowers ? please answer yes or no  ', 'yes/y or no/n').toLowerCase();
+  }
 
-if (loveflower === 'yes' || loveflower === 'y') {
-  alert('You are flower!');
-  count++;
+  if (loveflower === 'yes' || loveflower === 'y') {
+    alert('You are flower!');
+    count++;
 
-} else if (loveflower === 'no' || loveflower === 'n') {
-  alert('Okay! i love flower so much espicially pink flower');
+  } else if (loveflower === 'no' || loveflower === 'n') {
+    alert('Okay! i love flower so much espicially pink flower');
+  }
 }
+flowerLove(loveflower);
 alert('Answer of questions : My uniSpecialization is Computer engineer , My age 24 , My lovely color is gray , My favourite drink is coffee, I love flower very much');
 
 //Question 6
